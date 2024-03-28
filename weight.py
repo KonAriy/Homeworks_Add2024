7f191106d967dc8fc491def144f504c8b34fa0ef
KonAriy <165194261+KonAriy@users.noreply.github.com>
2024-03-28 22:36:11 +0500

    исправление кода

--- weight.py
+++ weight.py
@@ -1,11 +1,10 @@
 weight = int(input('Введите вес посылки (кг): '))
-a = 50
+rub = 50
 if weight <= 2:
-    print('Стоимость доставки ', a)
-if 2 < weight < 10:
-    for i in range(2, 10):
-        if weight == i:
-            a = a + (20 * (i - 2))
-            print('Стоимость доставки ', a)
-if weight >= 10:
-    print('Стоимость доставки ', a * 4)
+    rub = 50
+if weight < 10:
+    rub = 50 + (weight - 2) * 20
+else:
+    rub = 200
+
+print('Стоимость досьавки составит: ', rub, 'руб.')
